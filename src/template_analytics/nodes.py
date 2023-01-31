90d20e1f157d03f44fb92526769718b5ae290fed
robertcinca <robert.cinca.14@ucl.ac.uk>
2023-01-31 12:55:51 +0000

    added type conversion

--- src/template_analytics/nodes.py
+++ src/template_analytics/nodes.py
@@ -10,65 +10,88 @@ import numpy as np
 import pandas as pd
 
 
-def merge_data(data: pd.DataFrame, data_2: pd.DataFrame) -> pd.DataFrame:
-    return data.merge(data_2, on="ID")
+class DataNodes:
+    def merge_data(data: pd.DataFrame, data_2: pd.DataFrame) -> pd.DataFrame:
+        return data.merge(data_2, on="ID")
 
+    def convert_types(data: pd.DataFrame):
+        """
+        Convert data types to numeric and categorical columns
+        """
+        # Replace bool with numeric
+        data.replace({False: 0.0, True: 1.0}, inplace=True)
 
-def split_data(
-    data: pd.DataFrame, parameters: Dict[str, Any]
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
-    """Splits data into features and target training and test sets.
+        # Get a list of columns that are numeric and objects
+        numeric_list = data.select_dtypes(include=[np.number]).columns
+        object_list = data.select_dtypes(object).columns
 
-    Args:
-        data: Data containing features and target.
-        parameters: Parameters defined in parameters.yml.
-    Returns:
-        Split data.
-    """
+        # convert numeric types
+        data[numeric_list] = data[numeric_list].astype(np.float32)
 
-    data_train = data.sample(
-        frac=parameters["train_fraction"], random_state=parameters["random_state"]
-    )
-    data_test = data.drop(data_train.index)
+        # convert objects
+        for column in object_list:
+            data[column] = pd.Categorical(data[column])
+            data[column] = data[column].cat.codes
 
-    X_train = data_train.drop(columns=parameters["target_column"])
-    X_test = data_test.drop(columns=parameters["target_column"])
-    y_train = data_train[parameters["target_column"]]
-    y_test = data_test[parameters["target_column"]]
+        return data
 
-    return X_train, X_test, y_train, y_test
+    def split_data(
+        data: pd.DataFrame, parameters: Dict[str, Any]
+    ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+        """Splits data into features and target training and test sets.
 
+        Args:
+            data: Data containing features and target.
+            parameters: Parameters defined in parameters.yml.
+        Returns:
+            Split data.
+        """
 
-def make_predictions(X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series) -> pd.Series:
-    """Uses 1-nearest neighbour classifier to create predictions.
+        data_train = data.sample(
+            frac=parameters["train_fraction"], random_state=parameters["random_state"]
+        )
+        data_test = data.drop(data_train.index)
 
-    Args:
-        X_train: Training data of features.
-        y_train: Training data for target.
-        X_test: Test data for features.
+        X_train = data_train.drop(columns=parameters["target_column"])
+        X_test = data_test.drop(columns=parameters["target_column"])
+        y_train = data_train[parameters["target_column"]]
+        y_test = data_test[parameters["target_column"]]
 
-    Returns:
-        y_pred: Prediction of the target variable.
-    """
+        return X_train, X_test, y_train, y_test
 
-    X_train_numpy = X_train.to_numpy()
-    X_test_numpy = X_test.to_numpy()
+    def make_predictions(
+        X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.Series
+    ) -> pd.Series:
+        """Uses 1-nearest neighbour classifier to create predictions.
 
-    squared_distances = np.sum((X_train_numpy[:, None, :] - X_test_numpy[None, :, :]) ** 2, axis=-1)
-    nearest_neighbour = squared_distances.argmin(axis=0)
-    y_pred = y_train.iloc[nearest_neighbour]
-    y_pred.index = X_test.index
+        Args:
+            X_train: Training data of features.
+            y_train: Training data for target.
+            X_test: Test data for features.
 
-    return y_pred
+        Returns:
+            y_pred: Prediction of the target variable.
+        """
 
+        X_train_numpy = X_train.to_numpy()
+        X_test_numpy = X_test.to_numpy()
 
-def report_accuracy(y_pred: pd.Series, y_test: pd.Series):
-    """Calculates and logs the accuracy.
+        squared_distances = np.sum(
+            (X_train_numpy[:, None, :] - X_test_numpy[None, :, :]) ** 2, axis=-1
+        )
+        nearest_neighbour = squared_distances.argmin(axis=0)
+        y_pred = y_train.iloc[nearest_neighbour]
+        y_pred.index = X_test.index
 
-    Args:
-        y_pred: Predicted target.
-        y_test: True target.
-    """
-    accuracy = (y_pred == y_test).sum() / len(y_test)
-    logger = logging.getLogger(__name__)
-    logger.info("Model has accuracy of %.3f on test data.", accuracy)
+        return y_pred
+
+    def report_accuracy(y_pred: pd.Series, y_test: pd.Series):
+        """Calculates and logs the accuracy.
+
+        Args:
+            y_pred: Predicted target.
+            y_test: True target.
+        """
+        accuracy = (y_pred == y_test).sum() / len(y_test)
+        logger = logging.getLogger(__name__)
+        logger.info("Model has accuracy of %.3f on test data.", accuracy)
--- src/template_analytics/pipeline.py
+++ src/template_analytics/pipeline.py
@@ -5,32 +5,39 @@ generated using Kedro 0.18.4
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import make_predictions, merge_data, report_accuracy, split_data
+# from .nodes import make_predictions, merge_data, report_accuracy, split_data,
+from .nodes import DataNodes
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=merge_data,
+                func=DataNodes.merge_data,
                 inputs=["application_record_csv", "credit_record_csv"],
-                outputs="preprocessed_records",
-                name="merge",
+                outputs="merged_record",
+                name="merged",
             ),
             node(
-                func=split_data,
+                func=DataNodes.convert_types,
+                inputs="merged_record",
+                outputs="converted_record",
+                name="converted",
+            ),
+            node(
+                func=DataNodes.split_data,
                 inputs=["example_iris_data", "parameters"],
                 outputs=["X_train", "X_test", "y_train", "y_test"],
                 name="split",
             ),
             node(
-                func=make_predictions,
+                func=DataNodes.make_predictions,
                 inputs=["X_train", "X_test", "y_train"],
                 outputs="y_pred",
                 name="make_predictions",
             ),
             node(
-                func=report_accuracy,
+                func=DataNodes.report_accuracy,
                 inputs=["y_pred", "y_test"],
                 outputs=None,
                 name="report_accuracy",
